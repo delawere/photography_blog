f446a358bc6d540226ebe97a523b65cec506e83e
Dmitriy Zhiganov <delawere2009@yandex.ru>
2022-08-23 16:56:40 +0300

    WIP

---
+++ src/components/gallery.js
@@ -0,0 +1,9 @@
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Gallery = () => {
+  return <div>Gallery</div>
+}
+
+export default Gallery
